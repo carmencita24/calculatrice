2ccaf7598561bbdc959e2e65cedcb6efc75bc27d
Carmen Goerrian <carmengoerrian@yahoo.fr>
2023-10-18 15:14:43 +0200

    interface du CalculatorModelInterface

--- src/CalculatorModelInterface.java
+++ src/CalculatorModelInterface.java
@@ -1,5 +1,15 @@
 
 
 public interface CalculatorModelInterface {
+    public void add();
+	public void substract();
+	public void multiply();
+	public void divide();
+	public void opposite();
+	public void push();
+	public Double pop();
+	public void drop();
+	public void swap();
+	public void clear();
     
 }
