5812bf769a96697c8705e4298dba0fc48cbb88e8
Alan <alan.retailleau@gmail.com>
2023-10-18 15:31:53 +0200

    Controlleur

--- src/CalculatorController.java
+++ src/CalculatorController.java
@@ -1,3 +1,26 @@
-public class CalculatorController {
-    
+import java.util.Stack;
+
+public class CalculatorController implements CalculatorControlerInterface {
+	
+	CalculatorModel model = new CalculatorModel();
+	
+	public void change(String accumulateur) {
+		model.setAccu(accumulateur);
+	}
+	
+	public void change(Stack<Double> pile) {
+		model.setPile(pile);
+	}
+	
+	public void calculer() {
+		Stack<Double> pile = new Stack<Double>();
+		for(int i = 0; i < 3; i++) {
+			pile.add(2.0);
+		}
+		change(pile);
+		model.add();
+		model.multiply();
+		System.out.println(model.pop());
+	}
+	
 }
